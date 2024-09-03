2af5530ad84ea0d9140e81028ccf120faa233861
chaitupyata <chaitupyata@gmail.com>
2024-09-03 12:55:25 +0530

    Add getWatchHistory endPoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 
 
@@ -629,8 +630,68 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   .json(
     new ApiResponse(200, channel[0], "User channel fetched succesfully...... ")
   )
-
 })
+
+// 6). User watch histrory get cheyadam lookup dwara 
+
+
+const getWatchHistory = asyncHandler(async(req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "Videos",
+        toLocalField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owners",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+
+          // Front end ki Array return avuthundhi dhanni manam Object lekka convert chesthunnamu 
+
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200, 
+      user[0].watchHistory,
+      "Watch History fetched Successfully ................."
+    )
+  )
+})
+
 
 export {
   registerUser,
@@ -642,7 +703,9 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
+
 } 
 
 
